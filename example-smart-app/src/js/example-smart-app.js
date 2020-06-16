d570fa7e4a1738af9a2cd1ad4b7aaf94cf112118
susankoju <aksusankoju@gmail.com>
2020-06-16 11:37:10 +0545

    Add client_id in launch-smart-sandbox and launch-patient

--- example-smart-app/src/js/example-smart-app.js
+++ example-smart-app/src/js/example-smart-app.js
@@ -27,6 +27,7 @@
         $.when(pt, obv).done(function(patient, obv) {
           var byCodes = smart.byCodes(obv, 'code');
           var gender = patient.gender;
+          console.log(patient);
 
           var fname = '';
           var lname = '';
